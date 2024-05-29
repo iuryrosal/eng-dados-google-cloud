2388987c02e545230848410621731272771e7dc2
iuryrosal <iuryrosal@gmail.com>
2024-05-29 17:30:22 -0300

    Adiciona endpoint do Cloud Storage

--- api_cloudrun/main.py
+++ api_cloudrun/main.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import request, Response
+from api_cloudrun.src.cloud_storage import CloudStorage
 
 app = Flask(__name__)
-print("teste1")
+
 
 @app.route("/", methods=["GET"])
 def test_api():
-    print("teste2")
     """Return data of table in reporting dataset connected"""
     return "OK Funcionou", 200
+
+
+@app.route("/currencies", methods=["GET"])
+def pick_currencies():
+    """Return data of table in reporting dataset connected"""
+    try:
+        gcs = CloudStorage()
+        return gcs.pick_object("gs://csvs_to_sheets/1mYDfBCtj3GSm_H1vnocqEcQpcdP3tNy2rumtP4BRB7I.csv"), 200
+    except Exception as e:
+        return str(e), 500

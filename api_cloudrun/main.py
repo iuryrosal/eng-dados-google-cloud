03c90c591eb1e323103c595fb3af23a38b143fd5
iuryrosal <iuryrosal@gmail.com>
2024-05-29 20:49:54 -0300

    Atualiza api_cloudrun para captar os dados de moedas

--- api_cloudrun/main.py
+++ api_cloudrun/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from src.cloud_storage import CloudStorage
 
 app = Flask(__name__)
 
@@ -6,3 +7,10 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def health_check():
     return "OK Funcionou", 200
+
+
+@app.route("/curriencies", methods=["GET"])
+def get_curriencies():
+    gcs = CloudStorage(bucket_name="csvs_to_sheets")
+    str_content = gcs.pick_object(blob_name="1mYDfBCtj3GSm_H1vnocqEcQpcdP3tNy2rumtP4BRB7I.csv")
+    return str_content, 200
---
+++ api_cloudrun/src/cloud_storage.py
@@ -0,0 +1,9 @@
+from google.cloud import storage
+
+class CloudStorage:
+    def __init__(self,bucket_name) -> None:
+        self.client = storage.Client()
+        self.bucket = self.client.get_bucket(bucket_name)
+
+    def pick_object(self, blob_name):
+        return self.bucket.get_blob(blob_name).download_as_string()
